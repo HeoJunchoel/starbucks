4b399a5326138c4bf95882f4201c3360597d40cb
HeoJunCheol <hsgdf1234@naver.com>
2022-10-19 16:03:57 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// search
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 // BADGE
 const badgeEl = document.querySelector('header .badges')
@@ -170,6 +153,3 @@ spyEls.forEach(function(spyEl){
     .addTo(new ScrollMagic.Controller());
 });
 
-// 날짜지정
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear(); // 2022
